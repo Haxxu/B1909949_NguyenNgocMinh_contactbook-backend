d819aeac1d3cbf5c3c1805dd9b984729b8f384e7
Haxu <nguyenngocminh190901@gmail.com>
2023-01-16 13:36:39 +0700

    Cai dat cac handler truy xuat CSDL

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -2,6 +2,11 @@ const config = {
     app: {
         port: process.env.PORT || 3000,
     },
+    db: {
+        uri:
+            process.env.MONGODB_URI ||
+            "mongodb+srv://haxu:User12345!@cluster0.irctmzi.mongodb.net/?retryWrites=true&w=majority",
+    },
 };
 
 module.exports = config;
